748fd1cca3accc91c3b88061ffade2b94cc1b613
Jer <144748914+rjeremypaul@users.noreply.github.com>
2024-07-21 09:50:32 +0800

    Update App.js

--- reservation/src/App.js
+++ reservation/src/App.js
@@ -1,12 +1,34 @@
-// import './App.css';
-import FirstNavbar from "./FirstNavbar";
+import React, { useState } from 'react';
+import './App.css'; 
+import FirstNavbar from './FirstNavbar';
+import Login from './login';
+import Register from './register';
 
 function App() {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+  const toggleLoginForm = () => {
+    setShowLoginForm(!showLoginForm);
+    setShowRegisterForm(false); 
+  };
+
+  const toggleRegisterForm = () => {
+    setShowRegisterForm(!showRegisterForm);
+    setShowLoginForm(false); 
+  };
+
   return (
     <div className="App">
-      <FirstNavbar />
+      <FirstNavbar toggleLoginForm={toggleLoginForm} toggleRegisterForm={toggleRegisterForm} />
+      {showLoginForm && <Login />}
+      {showRegisterForm && <Register />}
     </div>
+
+    
   );
 }
+
+
 
 export default App;
