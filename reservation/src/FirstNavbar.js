4208d238b9b43a6e8c4d5f8bfe21b68bbb5c900b
Jer <144748914+rjeremypaul@users.noreply.github.com>
2024-07-21 09:52:00 +0800

    Update FirstNavbar.js

--- reservation/src/FirstNavbar.js
+++ reservation/src/FirstNavbar.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import './FirstNavbar.css';
 
-const FirstNavbar = () => {
-    return ( 
-        <nav className="navbar">
-            <div className="nav-title">iReserba</div>
-            <div className="navbar-links-left">
-                <ul className="navbar-links-no1">
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Contact us</a></li>
-                    <li><a href="#">Facilities</a></li>
-                </ul>
-            </div>
-            <div className="navbar-links-right">
-                <ul className="navbar-links-no2">
-                    <li><a href="#">Sign in</a></li>
-                    <li><a href="#">Sign up</a></li>
-                </ul>
-            </div>
-        </nav>
-     );
+const FirstNavbar = ({ toggleLoginForm, toggleRegisterForm }) => {
+  return (
+    <nav className="navbar">
+      <div className="nav-title">iReserba</div>
+      <div className="navbar-links-left">
+        <ul className="navbar-links-no1">
+          <li><a href="#">About us</a></li>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">Facilities</a></li>
+        </ul>
+      </div>
+      <div className="navbar-links-right">
+        <ul className="navbar-links-no2">
+          <li><a href="#" onClick={toggleLoginForm}>Sign in</a></li>
+          <li><a href="#" onClick={toggleRegisterForm}>Sign up</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
- 
+
 export default FirstNavbar;
